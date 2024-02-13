c255f2813ba07171fabbf248813398a2a15cebdc
fpoblet <91472594+fpoblet@users.noreply.github.com>
2024-02-13 17:37:39 +0100

    added lines to load files

--- SDn_MAARSY_v1.py
+++ SDn_MAARSY_v1.py
@@ -19,100 +19,6 @@ from extract_winds import extract_winds
 from structure_functions import calculate_structure_function_q, calculate_structure_function_q_loc
 
 
-# def extract_winds(ipath, year, month, daysn):
-#     U0 = []
-#     V0 = []
-#     U0mod = []
-#     heights = []
-#     date_list = []
-
-#     for y in year:
-#         for m in month:
-#             for d in range(1, daysn[m-1] + 1):
-#                 filename = ipath + '%04d/%04d%02d/VEL_h5/%04d%02d%02d_MST_MAARSY_Tropo_LTUT_windVEL_t15_v01c.h5' % (
-#                     y, y, m, y, m, d)
-
-#                 if os.path.isfile(filename):
-#                     with h5.File(filename, 'r') as f:
-#                         u = np.array(f.get('wind/u'))
-#                         v = np.array(f.get('wind/v'))
-#                         umod = np.sqrt(u**2 + v**2)
-                        
-#                         datenum = np.squeeze(f.get('info/datenums/'))
-#                         hours = np.squeeze(f.get('info/hour/')).astype(int)
-#                         mins = np.squeeze(f.get('info/min/')).astype(int)
-#                         secs = np.squeeze(f.get('info/sec/')).astype(int)
-#                         heights = np.array(f.get('info/alt'))
-                        
-#                         # Construct the base date for each file
-#                         base_date = datetime(y, m, d)
-#                         # Construct date_list for each file
-#                         date_list += [base_date.replace(hour=hour, minute=minute, second=second) for hour, minute, second in zip(hours, mins, secs)]
-
-#                         U0.append(u)
-#                         V0.append(v)
-#                         U0mod.append(umod)
-#                         # heights.append(heights)
-                
-#                 else:
-#                     print(f"File not found for {y}{m:02d}{d:02d}. Skipping...")
-
-#     if U0:
-#         U0 = np.concatenate(U0, axis=0)
-#         V0 = np.concatenate(V0, axis=0)
-#         U0mod = np.concatenate(U0mod, axis=0)
-#         # heights = np.concatenate(heights, axis=0)
-
-#         return U0, V0, U0mod, np.squeeze(heights), date_list
-
-
-
-
-
-# # routines to estimate high-order structure functions
-# def calculate_structure_function_q(wind_speed_data, q, max_tau):
-#     n = len(wind_speed_data)
-#     D_q = np.zeros(max_tau)
-
-#     for tau in range(1, max_tau + 1):               
-#         delta_v = wind_speed_data[:n - tau] - wind_speed_data[tau:]
-#         D_q[tau - 1] = np.nanmean(delta_v ** q)
-
-#     return D_q
-
-# def calculate_structure_function_q_loc(wind_speed_data, q, max_tau):
-#     n = len(wind_speed_data)
-#     D_q_loc = []
-
-#     for tau in range(1, max_tau + 1):
-#         r_values = []
-#         delta_v_q = []
-
-#         for t in range(n - tau):
-#             v_avg = np.mean(wind_speed_data[t:t + tau])
-#             r = v_avg * tau
-#             r_values.append(r)
-#             delta_v_q.append(np.abs(wind_speed_data[t] - wind_speed_data[t + tau]) ** q)
-
-#         r_values = np.array(r_values)
-#         delta_v_q = np.array(delta_v_q)
-
-#         # Calculate D_q^loc(r) by averaging in fifty bins every decade
-#         r_min = r_values.min()
-#         r_max = r_values.max()
-#         num_bins = 50
-#         bin_edges = np.logspace(np.log10(r_min), np.log10(r_max), num_bins + 1)
-#         D_q_loc_tau = []
-
-#         for i in range(num_bins):
-#             mask = (r_values >= bin_edges[i]) & (r_values < bin_edges[i + 1])
-#             if np.sum(mask) > 0:
-#                 D_q_loc_tau.append(np.mean(delta_v_q[mask]))
-
-#         D_q_loc.append(D_q_loc_tau)
-
-#     return D_q_loc
-
 def cube(x):
     if x >= 0:
         return x**(1/3)
@@ -120,14 +26,13 @@ def cube(x):
         return -(abs(x)**(1/3))
 
 
-year = [2019, 2020, 2021,2022]
-months = [1,2,3,4,5,6,7,8,9,10,11,12]
-dayssn = [31,28,31,30,31,30,31,31,30,31,30,31]
-
-# year = [2021]
+# year = [2019, 2020, 2021,2022]
 # months = [1,2,3,4,5,6,7,8,9,10,11,12]
 # dayssn = [31,28,31,30,31,30,31,31,30,31,30,31]
 
+year = [2022]
+months = [1,2,3,4,5,6,7,8,9,10,11,12]
+dayssn = [31,28,31,30,31,30,31,31,30,31,30,31]
 
 
 path = 'C:/Poblet/IAP/work/N-order_sf/MAARSY/'
@@ -140,13 +45,18 @@ U0, V0, WINDS, H, date_list =  extract_winds(ipath = path,
 
 
 # SAVE FILES
-with open('date_list.txt', 'w') as f:
+with open('C:/Poblet/IAP/work/N-order_sf/data/date_list_2022.txt', 'w') as f:
     f.write('\n'.join([date.strftime('%Y-%m-%d %H:%M:%S') for date in date_list]))
 
 
-np.save('WIND.npy', WINDS)
-np.save('height.npy', H)
+np.save('C:/Poblet/IAP/work/N-order_sf/data/WIND2022.npy', WINDS)
+np.save('C:/Poblet/IAP/work/N-order_sf/data/height.npy', H)
 
+
+# LOAD FILES
+date_list = [line.strip() for line in open('C:/Poblet/IAP/work/N-order_sf/data/date_list_2022.txt')]
+WINDS = np.load('C:/Poblet/IAP/work/N-order_sf/data/WIND2022.npy')
+H = np.load('C:/Poblet/IAP/work/N-order_sf/data/height.npy')
 
 
 ### Third order SFs plots
